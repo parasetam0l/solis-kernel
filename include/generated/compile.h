e644c9cb632c1da8a240a8ade64d8a5fe23b6858
Shaokun Zhang <zhangshaokun@hisilicon.com>
2016-06-21 15:32:57 +0800

    arm64: mm: remove page_mapping check in __sync_icache_dcache
    [ Upstream commit 20c27a4270c775d7ed661491af8ac03264d60fc6 ]
    
    __sync_icache_dcache unconditionally skips the cache maintenance for
    anonymous pages, under the assumption that flushing is only required in
    the presence of D-side aliases [see 7249b79f6b4cc ("arm64: Do not flush
    the D-cache for anonymous pages")].
    
    Unfortunately, this breaks migration of anonymous pages holding
    self-modifying code, where userspace cannot be reasonably expected to
    reissue maintenance instructions in response to a migration.
    
    This patch fixes the problem by removing the broken page_mapping(page)
    check from the cache syncing code, otherwise we may end up fetching and
    executing stale instructions from the PoU.
    
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: <stable@vger.kernel.org>
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Shaokun Zhang <zhangshaokun@hisilicon.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Sasha Levin <sasha.levin@oracle.com>

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 64 */
+/* This file is auto generated, version 65 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#64-Tizen SMP PREEMPT Wed Jan 26 12:33:24 +03 2022"
+#define UTS_VERSION "#65-Tizen SMP PREEMPT Wed Jan 26 12:50:51 +03 2022"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "los18pontesolo"
 #define LINUX_COMPILER "gcc version 4.9.3 (Ubuntu/Linaro 4.9.3-13ubuntu2) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,3 +1,3 @@
-#define UTS_RELEASE "3.18.36-g5f1595bf6e12-dirty"
+#define UTS_RELEASE "3.18.36-g81f39e4d57c3-dirty"
 #define OBS_RELEASE "localbuild"
 #define OBS_COMMIT_ID "-"
