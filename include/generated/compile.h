1edae05806fc8b044ae9165d844427776a718f29
Scott Bauer <sbauer@plzdonthack.me>
2016-06-23 08:59:47 -0600

    HID: hiddev: validate num_values for HIDIOCGUSAGES, HIDIOCSUSAGES commands
    [ Upstream commit 93a2001bdfd5376c3dc2158653034c20392d15c5 ]
    
    This patch validates the num_values parameter from userland during the
    HIDIOCGUSAGES and HIDIOCSUSAGES commands. Previously, if the report id was set
    to HID_REPORT_ID_UNKNOWN, we would fail to validate the num_values parameter
    leading to a heap overflow.
    
    Cc: stable@vger.kernel.org
    Signed-off-by: Scott Bauer <sbauer@plzdonthack.me>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>
    Signed-off-by: Sasha Levin <sasha.levin@oracle.com>

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 65 */
+/* This file is auto generated, version 66 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#65-Tizen SMP PREEMPT Wed Jan 26 12:50:51 +03 2022"
+#define UTS_VERSION "#66-Tizen SMP PREEMPT Wed Jan 26 12:56:18 +03 2022"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "los18pontesolo"
 #define LINUX_COMPILER "gcc version 4.9.3 (Ubuntu/Linaro 4.9.3-13ubuntu2) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,3 +1,3 @@
-#define UTS_RELEASE "3.18.36-g81f39e4d57c3-dirty"
+#define UTS_RELEASE "3.18.36-gaa4bd831350c-dirty"
 #define OBS_RELEASE "localbuild"
 #define OBS_COMMIT_ID "-"
