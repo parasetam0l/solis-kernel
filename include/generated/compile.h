5fc6405bbadcf02f9c421e3723712798ab853491
SKaya <skayadev@gmail.com>
2022-01-27 22:58:00 +0300

    v85

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 109 */
+/* This file is auto generated, version 110 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#109-Tizen SMP PREEMPT Thu Jan 27 22:42:48 +03 2022"
+#define UTS_VERSION "#110-Tizen SMP PREEMPT Thu Jan 27 22:56:56 +03 2022"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "los18pontesolo"
 #define LINUX_COMPILER "gcc version 4.9.3 (Ubuntu/Linaro 4.9.3-13ubuntu2) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,3 +1,3 @@
-#define UTS_RELEASE "3.18.80-gf3a156649234-dirty"
+#define UTS_RELEASE "3.18.85-gfe0903f30cb8-dirty"
 #define OBS_RELEASE "localbuild"
 #define OBS_COMMIT_ID "-"
