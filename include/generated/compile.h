4a3a22a18c697153c8b076d95c81eb378b44c741
SKaya <skayadev@gmail.com>
2022-01-27 14:11:26 +0300

    v38

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 74 */
+/* This file is auto generated, version 75 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#74-Tizen SMP PREEMPT Wed Jan 26 15:29:08 +03 2022"
+#define UTS_VERSION "#75-Tizen SMP PREEMPT Thu Jan 27 14:03:25 +03 2022"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "los18pontesolo"
 #define LINUX_COMPILER "gcc version 4.9.3 (Ubuntu/Linaro 4.9.3-13ubuntu2) "
