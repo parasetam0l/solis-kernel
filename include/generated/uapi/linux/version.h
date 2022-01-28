1a310e32ddafad3c9afdc900209e9e4ccfc579c5
SKaya <skayadev@gmail.com>
2022-01-28 15:40:30 +0300

    v120

--- include/generated/uapi/linux/version.h
+++ include/generated/uapi/linux/version.h
@@ -1,2 +1,2 @@
-#define LINUX_VERSION_CODE 201331
+#define LINUX_VERSION_CODE 201336
 #define KERNEL_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,3 +1,3 @@
-#define UTS_RELEASE "3.18.115-gd6fa8f751b3f-dirty"
+#define UTS_RELEASE "3.18.120-g72ca64113d87-dirty"
 #define OBS_RELEASE "localbuild"
 #define OBS_COMMIT_ID "-"
